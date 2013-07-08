4dc47aaa480d7dc75f57628b23c5b5884b794dbb
Abhishek Anand <aanandmailbox@gmail.com>
2013-07-08 06:02:14 +0000

    changed WEB.js to read index.html-take4

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
 var express = require('express');
-var js = require('fs'); //ReferenceError: fs is not defined
+var fs = require('fs'); //ReferenceError: fs is not defined
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    //    response.send('Hello World3!');
    
-   var buffer = fs.readFileSync('index.html');
-   response.send(buffer);
+   var indexhtml = fs.readFileSync('index.html');
+   var body = indexhtml
+   response.send(body);
 });
 
 var port = process.env.PORT || 5000;
