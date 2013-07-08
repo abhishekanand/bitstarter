2d78a4549591cff6bcb3d76692bf5551e22680fa
Abhishek Anand <aanandmailbox@gmail.com>
2013-07-08 05:20:46 +0000

    changed WEB.js to 3

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    response.send('Hello World3!');
+   // response.send(fs.readFileSync('./index.html','utf8'));
 });
 
 var port = process.env.PORT || 5000;
